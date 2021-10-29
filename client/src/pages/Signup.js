f276ae11229d4696fc9c5f37260cd69ff888f845
jfocha <jfocha@gmail.com>
2021-10-28 23:14:12 -0700

    Add Front-End User Authentication

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,7 +1,11 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 const Signup = () => {
   const [formState, setFormState] = useState({ username: '', email: '', password: '' });
+  const [addUser, { error }] = useMutation(ADD_USER);
 
   // update state based on form input changes
   const handleChange = (event) => {
@@ -13,11 +17,21 @@ const Signup = () => {
     });
   };
 
-  // submit form
-  const handleFormSubmit = async (event) => {
+  // submit form (notice the async!)
+  const handleFormSubmit = async event => {
     event.preventDefault();
-  };
 
+    // use try/catch instead of promises to handle errors
+    try {
+      // execute addUser mutation and pass in variable data from form
+      const { data } = await addUser({
+        variables: { ...formState }
+      });
+      Auth.login(data.addUser.token);
+    } catch (e) {
+      console.error(e);
+    }
+  };
   return (
     <main className='flex-row justify-center mb-4'>
       <div className='col-12 col-md-6'>
@@ -25,6 +39,7 @@ const Signup = () => {
           <h4 className='card-header'>Sign Up</h4>
           <div className='card-body'>
             <form onSubmit={handleFormSubmit}>
+              {error && <div>Sign up failed</div>}
               <input
                 className='form-input'
                 placeholder='Your username'
